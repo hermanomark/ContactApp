993b6ebe6dda183a67217c8e64c1ca3ef20e7526
unknown <hermano.mark.mh@gmail.com>
2018-08-16 12:48:57 +0800

    phonecall

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, List, ListItem, Text } from 'native-base';
+import { Container, Header, Content, List, ListItem, Text, Body} from 'native-base';
+import { phonecall } from 'react-native-communications'
 
 export default class ListExample extends Component {
 
+  tawag(number) {
+    phonecall(number, false)
+  }
+
   render() {
     const contacts = [
-      'Mark',
-      'Monique',
-      'Arvin',
-      'Ulysses',
-      'Kate'
+      {name: 'Mark', number: '09358645310'},
+      {name: 'Mark', number: '09358645310'},
+      {name: 'Mark', number: '09358645310'},
+      {name: 'Mark', number: '09358645310'}
     ]
 
     return (
@@ -18,10 +22,13 @@ export default class ListExample extends Component {
         <Content>
           <List>
             {
-              contacts.map((c) => {
+              contacts.map((c, i) => {
                 return(
-                  <ListItem>
-                    <Text> {c} </Text>
+                  <ListItem key={i} onPress={ () => { this.tawag() } }>
+                    <Body>
+                      <Text> {c.name} </Text>
+                      <Text note> {c.number} </Text>
+                    </Body>
                   </ListItem>
                   )
               })
